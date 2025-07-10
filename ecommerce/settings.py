a78a6ea3811e193e8a1f40030433755511e1ca95
saumya1317 <163722916+saumya1317@users.noreply.github.com>
2025-07-10 22:05:54 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-gel@ty0xfi^6_h*&8hn3@=p+)u6vge(@ke4jwab8&m!%rw&qzw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -137,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_16_char_app_password'  # Use your Gmail app password
+EMAIL_HOST_USER = 'saumya131725@gmail.com'
+EMAIL_HOST_PASSWORD = 'xwpc avbq qrhg fpbu'  # Use your Gmail app password
 DEFAULT_FROM_EMAIL = "no-reply@chickstyle.local"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -149,3 +148,4 @@ CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', CELERY_BROKER_UR
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
