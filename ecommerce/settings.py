58ae9b7f3f8560a5f8d62ceecfa91f0131c23a90
saumya1317 <163722916+saumya1317@users.noreply.github.com>
2025-07-10 22:12:56 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gel@ty0xfi^6_h*&8hn3@=p+)u6vge(@ke4jwab8&m!%rw&qzw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
